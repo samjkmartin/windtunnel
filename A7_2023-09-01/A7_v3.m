4b9f502c3bd77b56fd351bae1c38c13b9b9ae4fd
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2023-09-01 18:04:15 -0700

    Tweaked crank offsets in A7_v3

--- A7_2023-09-01/A7_v3.m
+++ A7_2023-09-01/A7_v3.m
@@ -1,6 +1,6 @@
 clc;
 clear all;
-% close all;
+close all;
 
 % Information about the disc
 D = 50; % diameter in mm
@@ -10,7 +10,7 @@ S = 20; % span in mm
 stations = [2:9];
 
 crankHeight = 3; % mm per crank
-crankOffsets = [33.5,33.5,33.5,33.5,33.5,33.5,33.5,33.5]; % to set position of r=0 for each disc
+crankOffsets = [33.5,33.5,33.5,33.5,33.5,33,33.5,33.5]; % to set position of r=0 for each disc
 
 FDnorm = zeros(length(stations),1); % placeholder for drag force normalized by Uinf and D
 uMax = 0.98; % u/Uinf threshold above which we do not include the data points in the drag calc
