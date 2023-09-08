1716cc2c713edcbcd1616954dcbd288ae5779e20
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2023-09-08 16:28:04 -0700

    Added more A7 data

--- A7_2023-09-08/A7_v4.m
+++ A7_2023-09-08/A7_v4.m
@@ -7,10 +7,10 @@ D = 50; % diameter in mm
 R = D/2; % Disc radius
 S = 20; % span in mm
 
-stations = [4];
+stations = [4,7];
 
 crankHeight = 3; % mm per crank
-crankOffsets = [33.5]; % to set position of r=0 for each disc
+crankOffsets = [33,33]; % to set position of r=0 for each disc
 
 FDnorm = zeros(length(stations),1); % placeholder for drag force normalized by Uinf and D
 uMax = 0.98; % u/Uinf threshold above which we do not include the data points in the drag calc
