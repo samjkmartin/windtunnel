7809f577e03f41a49d99e34aabd762a0630a18ca
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2023-09-08 16:02:34 -0700

    A7 re-testing a few stations

--- A7_2023-09-08/A7_v4.m
+++ A7_2023-09-08/A7_v4.m
@@ -1,16 +1,16 @@
 clc;
 clear all;
-% close all;
+close all;
 
 % Information about the disc
 D = 50; % diameter in mm
 R = D/2; % Disc radius
 S = 20; % span in mm
 
-stations = [2:9];
+stations = [4];
 
 crankHeight = 3; % mm per crank
-crankOffsets = [33.5,33.5,33.5,33.5,33.5,33,33.5,33.5]; % to set position of r=0 for each disc
+crankOffsets = [33.5]; % to set position of r=0 for each disc
 
 FDnorm = zeros(length(stations),1); % placeholder for drag force normalized by Uinf and D
 uMax = 0.98; % u/Uinf threshold above which we do not include the data points in the drag calc
