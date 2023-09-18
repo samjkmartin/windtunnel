6b3558e56c2cbbf67f5fb055dcef291c602e1408
raaghavt <96094982+raaghav-t@users.noreply.github.com>
2023-09-18 16:15:39 -0700

    a5s4

--- A5_2023-09-18/A5.m
+++ A5_2023-09-18/A5.m
@@ -7,10 +7,10 @@ D = 50; % diameter in mm
 R = D/2; % Disc radius
 S = 10; % span in mm
 
-stations = [1:3]; %[1:9];
+stations = 1:4; %[1:9];
 
 crankHeight = 3; % mm per crank
-crankOffsets = [36,35,34.75]; %[36, 36, 36, 36, 36, 36.5, 36, 36, 36]; % to set position of r=0 for each disc
+crankOffsets = [36,35,34.75,34.25]; %[36, 36, 36, 36, 36, 36.5, 36, 36, 36]; % to set position of r=0 for each disc
 
 FDnorm = zeros(length(stations),1); % placeholder for drag force normalized by Uinf and D
 uMax = 0.98; % u/Uinf threshold above which we do not include the data points in the drag calc
