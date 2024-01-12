775471f34f21d1406b4170b5949319a9a21e44ad
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-01-11 16:33:30 -0800

    drafting a new data processing file template that uses functions to generate the plots

---
+++ A7_2023-09-01/plotUR.m
@@ -0,0 +1,29 @@
+function figProfiles = plotUR(stations,S,D,uNorm,rNorm,uAxis,rAxis,sizeFont,sizeTitle)
+
+numStations = length(stations);
+
+figProfiles = figure; 
+for j=1:numStations
+    subplot(1,numStations,j);
+    plot(uNorm{j}, rNorm{j})
+    xlim(uAxis)
+    ylim(rAxis)
+    title(sprintf('x/D = %i', stations(j)))
+    xlabel('U/U_{\infty}')
+    if j==1
+        ylabel('r/D')
+    else
+        set(gca,'Yticklabel',[])
+    end
+
+    hold on
+    axval = axis;
+    axis([axval(1:3) -axval(3)])
+    plot(axval(1:2), [0 0], 'k:') % centerline
+    plot(uNorm{j}, -rNorm{j}, ':b'); % flipped profile
+end
+fontsize(sizeFont,'points')
+sgtitle(strcat('Normalized Velocity Profiles for S/D=', num2str(S/D)),'fontsize',sizeTitle)
+figProfiles.Position = [100 200 520*[2.63 1]*0.95]; 
+
+end
