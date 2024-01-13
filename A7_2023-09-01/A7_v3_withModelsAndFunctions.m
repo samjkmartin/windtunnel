aafd60f4d2d4fff3b8ed09d6e128eabab0c62be3
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-01-12 18:25:53 -0800

    functionalizing calculation of Dw, Sw, and Vw

--- A7_2023-09-01/A7_v3_withModelsAndFunctions.m
+++ A7_2023-09-01/A7_v3_withModelsAndFunctions.m
@@ -46,67 +46,11 @@ figOverlap = plotOverlap(stations,S,D,uNorm,rNorm,uAxis,rAxis,sizeFont,sizeTitle
 uMax = 0.98; % u/Uinf threshold above which we do not include the data points in the drag calc
 [CD, figCD] = dragCoeff(stations,S,D,uNorm,rNorm,uMax,14,14);
 
-[Vw, Dw, Sw] = meanWake(numStations,uNorm,rNorm,uMax);
+[Vw, Dw, Sw, figMeanWake] = meanWake(stations,uNorm,rNorm,uMax);
 
 %{
-
-%% Calculate wake diameter, span, and mean wake velocity
-Dw = zeros(numStations,1); 
-Sw = Dw;
-Vw = Dw; 
-for j=1:numStations
-    % finding outer wake boundaries
-    top = 1; 
-    while uNorm{j}(top)>=uMax
-        top = top+1; 
-    end
-    bottom = length(uNorm{j});
-    while uNorm{j}(bottom) >= uMax
-        bottom = bottom-1; 
-    end
-
-    % finding wake core boundaries
-    coreTop = top; 
-    while uNorm{j}(coreTop)<uMax
-        coreTop = coreTop+1; 
-    end
-    coreBottom = bottom;
-    while uNorm{j}(coreBottom)<uMax
-        coreBottom = coreBottom-1; 
-    end
-
-    % because the first set of while loops overshoot the outer edges
-    top = top-1; 
-    bottom = bottom+1; 
-
-    % wake diameter
-    Dw(j) = rNorm{j}(bottom)-rNorm{j}(top); 
-
-    % Check to see if the wake is annular or circular
-    isRing = 1; 
-    if coreTop >= coreBottom
-        isRing = 0; 
-    end
-
-    % calculating Sw (wake span) and Vw (area-based avg of uNorm inside wake) 
-    if isRing
-        Sw(j) = (rNorm{j}(coreTop)-rNorm{j}(top)+ rNorm{j}(bottom)-rNorm{j}(coreBottom))/2; 
-        uTop = uNorm{j}(top:coreTop);
-        rTop = rNorm{j}(top:coreTop);
-        uBottom = uNorm{j}(coreBottom:bottom);
-        rBottom = rNorm{j}(coreBottom:bottom);
-        I = pi*(trapz(rTop, uTop.*abs(rTop))+trapz(rBottom, uBottom.*rBottom)); % 2*pi*Integral is double-counting because we're using both positive and "negative" r, so we divide by 2
-        Aring = 0.25*pi*(Dw(j)^2-(Dw(j)-2*Sw(j))^2);
-        Vw(j) = I/Aring;
-    else
-        Sw(j) = Dw(j)/2;
-        I = pi*(trapz(rNorm{j}(top:bottom),abs(rNorm{j}(top:bottom)).*uNorm{j}(top:bottom)));
-        Aring = 0.25*pi*Dw(j)^2; 
-        Vw(j) = I/Aring;
-    end
-end
-
 %% Mean wake comparison with 1-D (tophat) entrainment models
+
 CT = mean(CD);
 EE = 0.12; 
 xe = 0.5; 
@@ -132,7 +76,7 @@ legend('Wind tunnel data',strcat('Full Model (E=',num2str(EE),', x_e=',num2str(x
 fontsize(sizeFont,'points')
 % title(strcat('Mean Wake Velocity for S/D=',num2str(S/D)),'FontSize',sizeTitle)
 figVw.Position = [100 200 520*[2.63 1]*0.95]; % powerpoint slide main textbox size is 11.5" by 5.2". For some reason, between MATLAB saving the file and importing it to PPT, some width is lost
-exportgraphics(figVw, strcat('SD0,', num2str(100*S/D), '_Vw.pdf'),'ContentType','vector','BackgroundColor','none')
+% exportgraphics(figVw, strcat('SD0,', num2str(100*S/D), '_Vw.pdf'),'ContentType','vector','BackgroundColor','none')
 
 % % testing out a range of entrainment coefficients
 % for i=1:4
--- A7_2023-09-01/meanWake.m
+++ A7_2023-09-01/meanWake.m
@@ -1,45 +1,46 @@
-function [Vw, Dw, Sw] = meanWake(numStations,uNorm,rNorm,uMax);
+function [Vw, Dw, Sw, figMeanWake] = meanWake(stations,uNorm,rNorm,uMax);
 
-Dw = zeros(numStations,1); 
+numStations = length(stations);
+Dw = zeros(numStations,1);
 Sw = Dw;
-Vw = Dw; 
+Vw = Dw;
 for j=1:numStations
     % finding outer wake boundaries
-    top = 1; 
+    top = 1;
     while uNorm{j}(top)>=uMax
-        top = top+1; 
+        top = top+1;
     end
     bottom = length(uNorm{j});
     while uNorm{j}(bottom) >= uMax
-        bottom = bottom-1; 
+        bottom = bottom-1;
     end
 
     % finding wake core boundaries
-    coreTop = top; 
+    coreTop = top;
     while uNorm{j}(coreTop)<uMax
-        coreTop = coreTop+1; 
+        coreTop = coreTop+1;
     end
     coreBottom = bottom;
     while uNorm{j}(coreBottom)<uMax
-        coreBottom = coreBottom-1; 
+        coreBottom = coreBottom-1;
     end
 
     % because the first set of while loops overshoot the outer edges
-    top = top-1; 
-    bottom = bottom+1; 
+    top = top-1;
+    bottom = bottom+1;
 
     % wake diameter
-    Dw(j) = rNorm{j}(bottom)-rNorm{j}(top); 
+    Dw(j) = rNorm{j}(bottom)-rNorm{j}(top);
 
     % Check to see if the wake is annular or circular
-    isRing = 1; 
+    isRing = 1;
     if coreTop >= coreBottom
-        isRing = 0; 
+        isRing = 0;
     end
 
-    % calculating Sw (wake span) and Vw (area-based avg of uNorm inside wake) 
+    % calculating Sw (wake span) and Vw (area-based avg of uNorm inside wake)
     if isRing
-        Sw(j) = (rNorm{j}(coreTop)-rNorm{j}(top)+ rNorm{j}(bottom)-rNorm{j}(coreBottom))/2; 
+        Sw(j) = (rNorm{j}(coreTop)-rNorm{j}(top)+ rNorm{j}(bottom)-rNorm{j}(coreBottom))/2;
         uTop = uNorm{j}(top:coreTop);
         rTop = rNorm{j}(top:coreTop);
         uBottom = uNorm{j}(coreBottom:bottom);
@@ -50,7 +51,16 @@ for j=1:numStations
     else
         Sw(j) = Dw(j)/2;
         I = pi*(trapz(rNorm{j}(top:bottom),abs(rNorm{j}(top:bottom)).*uNorm{j}(top:bottom)));
-        Aring = 0.25*pi*Dw(j)^2; 
+        Aring = 0.25*pi*Dw(j)^2;
         Vw(j) = I/Aring;
     end
+end
+
+figMeanWake = figure;
+plot(stations,Vw,'ko','MarkerFaceColor','k')
+xlim([0 stations(end)])
+ylim([0.5 1])
+xlabel('x/D')
+ylabel('V_w/V_{\infty}')
+
 end
