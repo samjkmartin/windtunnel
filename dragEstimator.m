83f985bded398d4ef0a7ea87cf0596e0f31f5c92
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-08-15 15:19:48 -0700

    drag force estimation for AL40v1

--- dragEstimator.m
+++ dragEstimator.m
@@ -7,8 +7,8 @@ rho = 1.18; % air density kg/m^3
 % Information about the disc
 D = 0.05; % diameter in m
 R = D/2; % Disc radius
-S = 0.015; % span in m
-CD = 0.8696; % drag coefficient
+S = 0.02; % span in m
+CD = 0.8; % drag coefficient
 
 FD = 0.5*rho*CD*U^2*pi*(R^2-(R-S)^2); % drag force in Newtons
 FDgrams = 1000*FD/9.81 % drag "force" in grams under 1g gravity
