de69e7a498d10a296a61b804039aca39976ecf78
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-08-15 18:51:17 -0700

    S3 data

--- dragEstimator.m
+++ dragEstimator.m
@@ -8,7 +8,7 @@ rho = 1.18; % air density kg/m^3
 D = 0.05; % diameter in m
 R = D/2; % Disc radius
 S = 0.02; % span in m
-CD = 0.8; % drag coefficient
+CD = 0.8668; % drag coefficient
 
 FD = 0.5*rho*CD*U^2*pi*(R^2-(R-S)^2); % drag force in Newtons
 FDgrams = 1000*FD/9.81 % drag "force" in grams under 1g gravity
