f89956f7004110508648906a14a347c0a8ec9557
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-08-20 23:01:41 -0700

    drag estimate for S/D=0.5

--- dragEstimator.m
+++ dragEstimator.m
@@ -7,8 +7,8 @@ rho = 1.18; % air density kg/m^3
 % Information about the disc
 D = 0.05; % diameter in m
 R = D/2; % Disc radius
-S = 0.02; % span in m
-CD = 0.8668; % drag coefficient
+S = R; % span in m
+CD = 0.85; % drag coefficient
 
 FD = 0.5*rho*CD*U^2*pi*(R^2-(R-S)^2); % drag force in Newtons
 FDgrams = 1000*FD/9.81 % drag "force" in grams under 1g gravity
