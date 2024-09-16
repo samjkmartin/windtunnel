9ab52d3fa384fb835a5784212eeec18c4b824e60
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-09-16 13:21:31 -0700

    fixed csv data extraction mistake, added plots for deltaP, deltaP/Pinf, and deltaU/Uinf

--- AL50v1/AL50v1_2024-08-23/sampleTimeConvergence.m
+++ AL50v1/AL50v1_2024-08-23/sampleTimeConvergence.m
@@ -17,10 +17,10 @@ data = readmatrix('AL50v1S4_5mins.csv');
 dt = 0.05; % for 20 Hz data from pressure transducer
 time = 0.05:0.05:300;
 
-voltage = data(4,:);
+voltage = data(4,6:end);
 meanvolts = mean(voltage)
 
-pInfty = mean(data(1,1:200));
+pInfty = mean(data(1,6:205));
 
 close
 
@@ -43,12 +43,26 @@ for j=1:length(sampleTimeRange)
     meanStDevs(j) = mean(stDevs); 
 end
 
-hold on
-% plot(sampleTimeRange, maxDiffs, sampleTimeRange, meanStDevs) % plot Delta p (inches of water)
-% ylabel('\Deltap (inches of water)')
-plot(sampleTimeRange, maxDiffs/meanvolts, sampleTimeRange, meanStDevs/meanvolts)
-ylabel('\Deltap/p')
+figure
+plot(sampleTimeRange, maxDiffs, sampleTimeRange, meanStDevs) % plot Delta p (inches of water)
+ylabel('\Deltap (inches of water)')
 xlabel('sample time (s)')
 title('Maximum excursion of recorded mean dynamic pressure from true mean')
-subtitle('S/D=0.5, 5 mins of data')
-legend('Crank 29', 'Crank 29, mean std dev of X-second sample')
+subtitle('S/D=0.5, solidity=0.6, 5 mins of data, Crank 29')
+legend('maximum excursion for X-second sample', 'mean std dev of X-second sample')
+
+figure
+plot(sampleTimeRange, maxDiffs/pInfty, sampleTimeRange, meanStDevs/pInfty)
+ylabel('\Deltap/p_{\infty}')
+xlabel('sample time (s)')
+title('Maximum excursion of recorded mean normalized pressure from true mean')
+subtitle('S/D=0.5, solidity=0.6, 5 mins of data, Crank 29')
+legend('maximum excursion of X-second sample', 'mean std dev of X-second sample')
+
+figure
+plot(sampleTimeRange, 0.5*maxDiffs/sqrt(meanvolts*pInfty), sampleTimeRange, 0.5*meanStDevs/sqrt(meanvolts*pInfty))
+ylabel('\DeltaU/U_{\infty}')
+xlabel('sample time (s)')
+title('Maximum excursion of recorded mean normalized velocity from true mean')
+subtitle('S/D=0.5, solidity=0.6, 5 mins of data, Crank 29')
+legend('maximum excursion of X-second sample', 'mean std dev of X-second sample')
