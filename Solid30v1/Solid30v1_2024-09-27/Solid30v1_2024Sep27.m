1064433398003a99d4767ff2d64c4218716e8a65
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2024-10-02 15:54:25 -0700

    changed xlim

--- Solid30v1/Solid30v1_2024-09-27/Solid30v1_2024Sep27.m
+++ Solid30v1/Solid30v1_2024-09-27/Solid30v1_2024Sep27.m
@@ -55,7 +55,7 @@ end
 %% Plotting and analyzing the data
 
 % Plot formatting (set manually)
-uAxis = [0.35 1]; % U axis values for all velocity profile plots
+uAxis = [0.65 1]; % U axis values for all velocity profile plots
 rAxis = [-1.75 1.75]; % r axis values for all velocity profile plots
 sizeFont = 20; % default font size for multi-panel figures
 sizeTitle = 24; % default title font size for multi-panel figures
