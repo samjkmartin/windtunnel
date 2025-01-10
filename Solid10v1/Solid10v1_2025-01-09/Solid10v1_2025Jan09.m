c817ad3f83d5774e4c5ab80341200d2d0744dac9
Sam Kaufman-Martin <50849897+samjkmartin@users.noreply.github.com>
2025-01-09 17:54:43 -0800

    plotted S4 data and comparison plots with Oct 24

--- Solid10v1/Solid10v1_2025-01-09/Solid10v1_2025Jan09.m
+++ Solid10v1/Solid10v1_2025-01-09/Solid10v1_2025Jan09.m
@@ -22,11 +22,11 @@ R = D/2; % Disc radius
 %% Importing the raw data and saving to MATLAB variables
 % Set crankOffsets manually
 
-stations = [4:6];
+stations = [4];
 numStations = length(stations);
 
 crankHeight = 3; % mm per crank
-crankOffsets = [34,32,33.75]; % to set position of r=0 for each wake station (units: number of cranks from probe's starting position)
+crankOffsets = [34.25]; % to set position of r=0 for each wake station (units: number of cranks from probe's starting position)
 
 cranks = cell(numStations,1); 
 r = cranks; 
